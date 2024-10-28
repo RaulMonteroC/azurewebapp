80d2eeac20411d8e82e1cbece1fabc68d6919764
Raul Montero <raul@monteroc.com>
2024-10-28 12:14:11 -0400

    Add application insights and custom configuration setting

--- AzureApp/Controllers/HomeController.cs
+++ AzureApp/Controllers/HomeController.cs
@@ -4,17 +4,12 @@ using AzureApp.Models;
 
 namespace AzureApp.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger,
+                            IConfiguration configuration) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
+        ViewBag.Greetings = configuration["Greetings"]!;
         return View();
     }
 
